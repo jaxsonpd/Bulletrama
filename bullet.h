c71c0fa3e2dc2003f0f1abb56ad9f19e834bd425
Ben Gunn <bgu37@uclive.ac.nz>
2022-10-10 12:07:44 +1300

    first work on bullet h file

---
+++ bullet.h
@@ -0,0 +1,45 @@
+/**
+ * @file bullet.h
+ * @author ben gunn (bgu37)
+ * @brief contains the structs for bullet position, speed and owner
+ * @date 2022-10-10
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef BULLET_H
+#define BULLET_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+
+
+typedef struct {
+    uint8_t x;
+    uint8_t y;
+    uint8_t owner;
+    uint8_t speed;
+     
+} Bullet_t;
+
+/** Set the Bullet variables
+ * @param BulletSpeed the speed the bullet moves at
+ */
+void BulletConfig(uint8_t speed);
+
+/** Intialise the bullet
+ * @param x x position of the bullet
+ * @param y y position of the bullet
+ * @param owner direction of bullet and ownership 0 == enemy
+ * @return Bullet_t struct
+ */
+Bullet_t BulletInit(uint8_t x, uint8_t y, uint8_t owner);
+
+/** Check to see if the player needs to be moved
+ * @param player pointer to a Player_t struct
+ */
+void playerUpdate(Player_t* player);
+
+#endif
